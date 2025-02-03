119114eeb316dc5359781b2d31d302ea9a642ea1
sreesha1502 <sreesha1523@gmail.com>
2025-02-03 18:52:53 +0100

    initial commit

---
+++ src/test/java/com/vegobject/vegobject/VegobjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegobject.vegobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegobjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
