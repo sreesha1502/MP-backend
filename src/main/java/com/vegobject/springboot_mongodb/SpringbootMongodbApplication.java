64b6fff31a6b542defcba5467cd55c8d4cabc51e
sreesha1502 <sreesha1523@gmail.com>
2025-02-09 14:32:24 +0100

    testing to get rid of white label error

--- src/main/java/com/vegobject/springboot_mongodb/SpringbootMongodbApplication.java
+++ src/main/java/com/vegobject/springboot_mongodb/SpringbootMongodbApplication.java
@@ -2,12 +2,21 @@ package com.vegobject.springboot_mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+// @RestController
 public class SpringbootMongodbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMongodbApplication.class, args);
 	}
+
+	// @GetMapping
+	// public String SayHello() {
+	// 	return "hello springboot";
+	// }
 
 }
