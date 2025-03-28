eab9edbb54e1cdfa3cdb00ac8e88c0d2ad406ed7
sreesha1502 <sreesha1523@gmail.com>
2025-03-28 20:59:28 +0100

    remove Geometry class; update Poles class to include Geometry serialization and deserialization; add new _Geometry class

--- src/main/java/com/vegobject/springboot_mongodb/collection/Poles.java
+++ src/main/java/com/vegobject/springboot_mongodb/collection/Poles.java
@@ -1,9 +1,15 @@
 package com.vegobject.springboot_mongodb.collection;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
+import org.springframework.data.mongodb.core.mapping.Document;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.vegobject.springboot_mongodb.serializer.GeometryDeserializer;
+import com.vegobject.springboot_mongodb.serializer.GeometrySerializer;
+
+import org.locationtech.jts.geom.Geometry;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,7 +17,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @Document(collection = "poles")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
@@ -22,11 +27,14 @@ public class Poles {
     private String id;
     private double altitude;
     private int speed;
-    private Geometry gps;
     private int fixType;
     private double courseOverGround;
     private double hdop;
     private String capturedDate;
+
+    @JsonSerialize(using = GeometrySerializer.class)
+    @JsonDeserialize(using = GeometryDeserializer.class)
+    private Geometry gps;
 
 
 }
