27a03ba45f91f8e1c11f8f0b3d757eaa08ad7560
sreesha1502 <sreesha1523@gmail.com>
2025-03-30 16:37:15 +0200

    remove person controller methods

--- src/main/java/com/vegobject/springboot_mongodb/controller/PersonController.java
+++ src/main/java/com/vegobject/springboot_mongodb/controller/PersonController.java
@@ -1,30 +1,30 @@
 package com.vegobject.springboot_mongodb.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.vegobject.springboot_mongodb.collection.Person;
-import com.vegobject.springboot_mongodb.service.PersonService;
+// import com.vegobject.springboot_mongodb.collection.Person;
+// import com.vegobject.springboot_mongodb.service.PersonService;
 
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
-    @Autowired
-    private PersonService personService;
+    // @Autowired
+    // private PersonService personService;
 
-    @PostMapping
-    public String savePerson(@RequestBody Person person) {
-        return personService.savePerson(person);
+    // @PostMapping
+    // public String savePerson(@RequestBody Person person) {
+    //     return personService.savePerson(person);
         
-    }
+    // }
 
-    @GetMapping
-    public String getPerson() {
-        return personService.getPerson();
-    }
+    // @GetMapping
+    // public String getPerson() {
+    //     return personService.getPerson();
+    // }
 }
