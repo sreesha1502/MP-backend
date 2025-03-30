e5c6ce708753c95105df1f115b5a5be7ca4e0487
sreesha1502 <sreesha1523@gmail.com>
2025-03-30 16:37:32 +0200

    refactor PolesController to use RequestMethod for clarity in endpoint definitions

--- src/main/java/com/vegobject/springboot_mongodb/controller/PolesController.java
+++ src/main/java/com/vegobject/springboot_mongodb/controller/PolesController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vegobject.springboot_mongodb.collection.CapturedDates;
@@ -26,12 +27,13 @@ public class PolesController {
     }
 
     @GetMapping("/{cdate}")
+    @RequestMapping(value = "/{cdate}", method= RequestMethod.GET)
     public Poles[] getPolesByDate(@PathVariable long cdate) {
         return polesService.getPolesByDate(cdate);
     }
 
     @GetMapping
-    @RequestMapping("/capturedDates")
+    @RequestMapping(value = "/capturedDates", method = RequestMethod.GET)
     public CapturedDates getCapturedDateStrings() {
         return polesService.getCapturedDateStrings();
     }
